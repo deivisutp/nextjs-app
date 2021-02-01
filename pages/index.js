6b118f57824ad36e7e6a8fb1bf619e1e43075125
Deivis <deivisutp@gmail.com>
2021-02-01 20:17:44 -0300

    vercel dev-test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <h2>Enjoy Vercel features and services.</h2>
+        <h2>Enjoy Vercel features and cloud services.</h2>
       </main>
 
     </div>
